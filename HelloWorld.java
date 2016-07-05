04558e093d604cf4ddbee63826b8f9ba99447a05
newman101 <jremes@outlook.com>
2016-07-05 20:47:09 +0100

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ *  This is the first Java exercise
+ *  Mainly written to test the configuration
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
