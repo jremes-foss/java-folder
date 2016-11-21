7008edb8d01174d4308dbd7643a38af4ac2fad6f
Juha Remes <jremes@outlook.com>
2016-11-21 14:48:19 +0000

    Added comment to Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -16,6 +16,9 @@
      x = multiply.nextInt();
      System.out.println("Multiplication table of number "+x+" is:");
 
+     // Multiply x * y, where y = 1,...,10. This is achieved most easily
+     // with a simple for loop.
+
      for (y = 1; y <= 10; y++) {
        System.out.println(x+"*"+y+" = "+(x*y));
      }
