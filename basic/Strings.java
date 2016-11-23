00f488700aac15529427dcb32985611bdb7b7d6f
Juha Remes <jremes@outlook.com>
2016-11-23 15:13:49 +0000

    Added basic/String.java + Tests

---
+++ basic/Strings.java
@@ -0,0 +1,28 @@
+/**
+ * Demonstrates various string operations.
+ *
+ * @author Juha Remes
+ *
+ */
+
+public class Strings {
+
+  /**
+   *  Removes a character from a string
+   *  @param str
+   *  @param pos
+   *  @return removedStr
+   *
+   */
+
+  public static String removeCharacter(String str, int pos) {
+
+    String removedStr = str.substring(0, pos) + str.substring(pos + 1);
+
+    if (str.length() == 0 || str == null) {
+      removedStr.equals("");
+    }
+
+    return removedStr;
+  }
+}
---
+++ basic/test/StringsTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringsTest {
+
+  @Test
+  public void removeCharacterTest() {
+
+    // Initialize the class under test
+    Strings tester = new Strings();
+
+    // Assertions
+    assertEquals("Hllo", tester.removeCharacter("Hello", 1));
+    assertEquals("", tester.removeCharacter("", 2));
+  }
+}
